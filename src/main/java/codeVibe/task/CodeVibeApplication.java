fdc62fae3df46c079b14b2d11f6841e3e2d71315
bokekez <bokekez@gmail.com>
2024-08-28 11:11:34 +0200

    refactored application file

--- src/main/java/codeVibe/task/CodeVibeApplication.java
+++ src/main/java/codeVibe/task/CodeVibeApplication.java
@@ -4,24 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
-import codeVibe.task.repository.UserDAO;
-import codeVibe.task.service.Services;
-import org.springframework.beans.factory.annotation.Value;
 
 import codeVibe.task.model.Migration;
-import codeVibe.task.repository.UserDAO;
 import codeVibe.task.seeders.RunSeeder;
 
 @SpringBootApplication
 public class CodeVibeApplication {
-	@Value("${spring.datasource.url}")
-	private String connectionString;
-
-	@Value("${spring.datasource.username}")
-	private String connectionUsername;
-
-	@Value("${spring.datasource.password}")
-	private String connectionPassword;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeVibeApplication.class, args);
